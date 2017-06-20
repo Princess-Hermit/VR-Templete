3bcc72ac61f964a9983ff83fc3ba2d6ffe9a94b2
Joshua-J <Joshua@Joshuas-Air.attlocal.net>
2017-06-20 13:54:59 -0700

    VR Templete
    The player can look around to trigger interactions.

---
+++ Assets/Fox_LookToPreformAction.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox_LookToPreformAction : MonoBehaviour
+{
+	public GameObject selectableObject;
+	public GameObject fox;
+	public long activateTimeSec;
+
+	private int itemSelected = 0;
+	private int timer = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Get the positions from the objects
+		Vector3 player_vector = this.transform.position;
+		Vector3 object_vector = selectableObject.transform.position;
+
+		//Create the two raycasts and get the objects they hit
+		RaycastHit headset_hit;
+		Physics.Raycast(player_vector, this.transform.forward, out headset_hit);//headset_point_vector - player_vector
+
+		//Calculate time sence 1970
+		System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+		int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+
+		try
+		{
+			if (headset_hit.collider.gameObject == selectableObject)
+			{
+				if(timer == 0)
+				{
+					timer = cur_time;
+				}
+			}
+			else
+			{
+				timer = 0;
+			}
+		}
+		catch(System.Exception e)
+		{
+			timer = 0;
+		}
+
+		if(timer != 0 && cur_time - timer == activateTimeSec)
+		{
+			print("Preform Action");
+			//selectableObject.SetActive (false); // Example Action
+			//Change the target of the Foxes
+			//for (int i = 0; i < foxes.Length; i++)
+			//{
+			fox.GetComponent<MalbersAnimations.AnimalAIControl>().target = this.transform;
+			selectableObject.GetComponent<AudioSource> ().Play();
+			//}
+		}
+		
+	}
+}
---
+++ Assets/Raycast.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Beat_Puzzle_Raycast : MonoBehaviour
+{
+    public GameObject hidden_player_point;
+    public GameObject hidden_object_point;
+    public GameObject Exit;
+
+    public AudioSource completion_music;
+    public Light completion_light;
+
+    private bool raycast_triggered = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the positions from the objects
+        Vector3 player_vector = this.transform.position;
+        Vector3 headset_point_vector = hidden_player_point.transform.position;
+        Vector3 object_point_vector = hidden_object_point.transform.position;
+
+        //Create the two raycasts and get the objects they hit
+        RaycastHit headset_hit;
+        RaycastHit object_hit;
+
+        Physics.Raycast(player_vector, this.transform.forward, out headset_hit);//headset_point_vector - player_vector
+        Physics.Raycast(player_vector, object_point_vector - player_vector, out object_hit);
+
+        //See if the player has solved the puzzle.
+        if ((headset_hit.collider.gameObject == hidden_player_point) && (object_hit.collider.gameObject == hidden_player_point))
+        {
+            if (raycast_triggered == false)
+            {
+                //Play the puzzle finished music
+                completion_music.Play();
+
+                //Play the light animation
+                completion_light.GetComponent<Animation>().Play();
+
+                //Make portal visible
+                Exit.SetActive(true);
+
+                //Make sure the play can't trigger the music and animation again
+                raycast_triggered = true;
+            }
+            Debug.Log("Raycast has triggered. Go to Next Scene");
+        }
+
+
+    }
+}
